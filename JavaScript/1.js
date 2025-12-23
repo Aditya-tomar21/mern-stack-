a1e289a4400b04c171085adcdf483a29b7f471c1
Aditya Singh Tomar <114979998+Aditya-tomar21@users.noreply.github.com>
2025-12-23 15:52:07 +0000

    JS-accenture

---
+++ JavaScript/1.js
@@ -0,0 +1,72 @@
+//----------------------------------------         "let" & "const" are block scoped and should be used more 
+// ---------------      module1 -  JS variables
+/*
+let a = 8;
+if(a==8)
+{
+    let a = 10;
+    console.log(a);
+    
+}
+console.log(a);
+
+const val = 99;
+// val =100;
+console.log(val)
+ */
+
+
+
+
+
+
+//----------------------------------------------------  Module2 - JS operators  ------------------------------------->
+// EX:1,  pg-18
+
+/* let item1 = 500;
+let item2 = 100;
+
+function discount (item1 , item2){
+let finalPrice = (item1 +item2) - ( ((item1+item2)/100)*10 )
+ return finalPrice;
+}
+
+
+let Price = discount(item1, item2);
+console.log("price after discount is" , `${Price}`);
+
+Price>=500?(console.log("final pay",` ${Price}`) ) : (console.log("Final pay" , `${Price +10 }`));
+
+
+*/
+
+
+
+
+//----------------------------------------------------    data types  ------------------------------------->
+// Ex:2 , pg-25
+
+// ---------------- NON Primitive data type - og change hoga 
+/*
+let arr = ["1" , "hello" , "3"]
+ function
+ let person = {
+    name:"aditya",
+    age:24
+ };
+let today = new Date();
+console.log(today);
+*/
+
+//--------------- primitve data types - og change nahi hoga
+/*
+
+ let a = 69;
+ let str = "hello world "
+ let ans = true;
+ let b = null;
+ let x ;    //undefined
+
+*/
+
+
